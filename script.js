e094c26e9052e3ad92e0eb9e02e2c64441d25600
pavelkazancev02 <pavelkazancev02@gmail.com>
2019-08-27 00:57:26 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const MyContract = artifacts.require("./MyContract.sol");
-
-module.exports = async function(callback) {
-  const contract = await MyContract.deployed()
-  const value = await contract.get()
-  console.log("Value:", value)
-}
